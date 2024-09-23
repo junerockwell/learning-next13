a5dbdbdfc5d8395535cb8c68ab7f3484420f241b
junerockwell <devapprocks@gmail.com>
2024-09-23 18:36:49 -0500

    Added About pages.

---
+++ app/about/page.jsx
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: "About Page",
+  description: "A site to learn NextJS 13 with DaisyUI",
+  keywords:
+    "Next, NextJS, NextJS 13, Next 13, React, ReactJS, DaisyUI, Developer, Frontend",
+};
+
+export default function AboutPage() {
+  return <div>About Page</div>;
+}
