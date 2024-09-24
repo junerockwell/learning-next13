68b9199e0b50b410df4e82a1a4ea393222dde331
junerockwell <devapprocks@gmail.com>
2024-09-24 15:39:05 -0500

    Rendering Books component on homepage.

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,11 @@
 import Link from "next/link";
+import Books from "./components/Books";
 
 export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <button className="btn btn-primary">Button</button>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/about/contact">Contact</Link>
-        </li>
-      </ul>
+      <Books />
     </div>
   );
 }
