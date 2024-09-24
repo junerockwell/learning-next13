6e3be3d223ef5d534a6c24ccb434142b431bf44c
junerockwell <devapprocks@gmail.com>
2024-09-24 16:03:28 -0500

    Adding uuid for book id.

--- app/api/books/route.js
+++ app/api/books/route.js
@@ -1,6 +1,7 @@
 import books from "./data.json";
 
 import { NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid";
 
 export async function GET(request) {
   return NextResponse.json(books);
@@ -8,13 +9,14 @@ export async function GET(request) {
 
 export async function POST(request) {
   const { title, link, img } = await request.json();
+  const id = uuidv4(); //books.length + 1,
   const newBook = {
-    id: books.length + 1,
+    id,
     title,
     link,
     img,
   };
 
   books.push(newBook);
-  return NextResponse.json("Book added successfully");
+  return NextResponse.json({ msg: "Book added successfully", id });
 }
