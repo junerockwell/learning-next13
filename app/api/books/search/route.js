8dde2465d6a1fece1c94b701b79a5fd7c89cfd62
junerockwell <devapprocks@gmail.com>
2024-09-24 15:38:50 -0500

    Created book search route endpoints.

---
+++ app/api/books/search/route.js
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import books from "../data.json";
+
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  console.log("searchparams: ", searchParams);
+  const query = searchParams.get("query");
+
+  const filteredBooks = books.filter((book) => {
+    return book.title.toLowerCase().includes(query.toLowerCase());
+  });
+
+  return NextResponse.json(filteredBooks);
+}
