e12a6f6263e6660b8124aa8472dd1e1dadf5360a
junerockwell <devapprocks@gmail.com>
2024-09-24 15:53:01 -0500

    Create AddBook.jsx

---
+++ app/components/AddBook.jsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useState } from "react";
+
+export default function AddBook() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [newBookTitle, setNewBookTitle] = useState("");
+
+  async function handleSubmitNewBook(e) {
+    e.preventDefault();
+    console.log("handleSubmitNewBook: ", newBookTitle);
+
+    const res = await fetch(`/api/book/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: newBookTitle,
+        link: "https://www.amazon.com/dp/B0979MGJ5J",
+        img: "https://via.placeholder.com/600/92c952",
+      }),
+    });
+
+    if (res.ok) {
+      setNewBookTitle("");
+      setModalOpen(false);
+    }
+  }
+
+  return (
+    <div className="mb-2">
+      <button className="btn" onClick={() => setModalOpen(true)}>
+        Add Book
+      </button>
+
+      <dialog
+        id="my_modal_3"
+        className={`modal ${modalOpen ? "modal-open" : ""}`}
+      >
+        <form
+          method="dialog"
+          className="modal-box"
+          onSubmit={handleSubmitNewBook}
+        >
+          <button
+            onClick={() => setModalOpen(false)}
+            htmlFor="my-modal-3"
+            className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+          >
+            x
+          </button>
+          <h3 className="font-bold text-lg">Add New Book</h3>
+          <input
+            type="text"
+            value={newBookTitle}
+            placeholder="Enter New Book Title"
+            className="input input-bordered w-full max-w-xs"
+            onChange={(e) => setNewBookTitle(e.target.value)}
+          />
+          <button type="submit" className="btn btn-primary">
+            Add Book
+          </button>
+        </form>
+      </dialog>
+    </div>
+  );
+}
